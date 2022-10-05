e302d157af71691831d9b821c2f0198fcfc2a340
verkesfl <floreverkest1@gmail.com>
2022-10-05 16:09:14 +0200

    webplatform

--- getimage/views.py
+++ getimage/views.py
@@ -44,16 +44,27 @@ def image(request):
         iiif_manifest = data_json["sequences"][0]['canvases'][0]["images"][0]["resource"]["@id"]
         license = data_json["sequences"][0]['canvases'][0]['images'][0]['license']
         label = data_json["label"]['@value']
+        objectnumber = data_json['@id']
+        label = data_json["label"]['@value']          
         if 'stam' in manifest:
+            objectnumber = objectnumber.split("stam:", 1)[1]
+            webplatform = 'https://data.collectie.gent/entity/stam:' + objectnumber
             instelling = 'STAM'
         elif 'hva' in manifest:
+            objectnumber = objectnumber.split("hva:", 1)[1]
+            webplatform = 'https://data.collectie.gent/entity/hva:' + objectnumber
             instelling = 'Huis van Alijn'
         elif 'dmg' in manifest:
+            objectnumber = objectnumber.split("dmg:", 1)[1]
+            webplatform = 'https://data.collectie.gent/entity/dmg:' + objectnumber
             instelling = 'Design Museum Gent'
         elif 'industriemuseum' in manifest:
+            objectnumber = objectnumber.split("industriemuseum:", 1)[1]
+            webplatform = 'https://data.collectie.gent/entity/industriemuseum:' + objectnumber
             instelling = 'Industriemuseum'
         else:
+            objectnumber = objectnumber.split("archiefgent:", 1)[1]
+            webplatform = 'https://data.collectie.gent/entity/archiefgent:' + objectnumber
             instelling = 'Archief Gent'
-        return render(request, 'image.html', {'iiif_manifest': iiif_manifest, 'instelling': instelling, 'label': label, 'license': license})
+        return render(request, 'image.html', {'iiif_manifest': iiif_manifest, 'instelling': instelling, 'label': label, 'license': license, 'webplatform': webplatform})
 
-# Create your views here.
